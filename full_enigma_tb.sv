83a9a87d713c0eebfb003930b419a9846c9f2f0f
zakdam <zakirov110594@gmail.com>
2014-08-03 16:12:51 +0400

    Update full_enigma_tb.sv

--- full_enigma_tb.sv
+++ full_enigma_tb.sv
@@ -6,7 +6,7 @@ logic       rst_i;
 logic [3:0] symb_numb;
 //logic [5:0] output_s;    //receiving encoded symbol from enigma
 //logic [5:0] input_s;     //generatin incoming symbol for enigma
-
+//logic [5:0] out_symb_o;
 
 //default RAM ports
 logic       dr_we_i;
@@ -14,7 +14,11 @@ logic [5:0] dr_data_i;
 logic [3:0] dr_addr_i;
 logic [5:0] dr_data_o;
 
-logic [5:0] trans;
+//final RAM ports
+logic [5:0] fr_data_o;
+
+logic [5:0] trans1;        //transition between input_s and in_symb_i
+logic [5:0] trans2;        //transition between output_s and out_symb_o
 
 //clock
 initial                               
@@ -37,7 +41,7 @@ initial
 //symbols' number
 initial
   begin
-    symb_numb = 4'd2;
+    symb_numb = 4'd6;
   end
 
 //------------default RAM signal definition  
@@ -45,91 +49,240 @@ initial
 initial
   begin
     #20;
+    
     @(posedge clk_i);
     dr_we_i = 1'b1;
     #20;
     @(posedge clk_i);
     dr_we_i = 1'b0;
     #80;
+    
     @(posedge clk_i);
     dr_we_i = 1'b1;
     #20;
     @(posedge clk_i);
     dr_we_i = 1'b0;
     #80;
+    
     @(posedge clk_i);
     dr_we_i = 1'b1;
     #20;
     @(posedge clk_i);
     dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
+    @(posedge clk_i);
+    dr_we_i = 1'b1;
+    #20;
+    @(posedge clk_i);
+    dr_we_i = 1'b0;
+    #80;
+    
   end
   
 //incoming data definition
 initial
   begin
     #20;
+    
+    //E
+    @(posedge clk_i);
+    dr_data_i = 6'd5;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
+    //N
+    @(posedge clk_i);
+    dr_data_i = 6'd14;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
+    //I
+    @(posedge clk_i);
+    dr_data_i = 6'd9;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
+    //G
+    @(posedge clk_i);
+    dr_data_i = 6'd7;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
+    //M
+    @(posedge clk_i);
+    dr_data_i = 6'd13;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
+    //A
     @(posedge clk_i);
     dr_data_i = 6'd1;
     #20;
     @(posedge clk_i);
     dr_data_i = 6'd0;
     #80;
+    
+    //Q
     @(posedge clk_i);
-    dr_data_i = 6'd2;
+    dr_data_i = 6'd17;
     #20;
     @(posedge clk_i);
     dr_data_i = 6'd0;
     #80;
+    
+    //W
     @(posedge clk_i);
-    dr_data_i = 6'd3;
+    dr_data_i = 6'd23;
     #20;
     @(posedge clk_i);
     dr_data_i = 6'd0;
+    #80;
+    
+    //E
+    @(posedge clk_i);
+    dr_data_i = 6'd5;
+    #20;
+    @(posedge clk_i);
+    dr_data_i = 6'd0;
+    #80;
+    
   end  
   
 //address definition
 initial
   begin
     #20;
+    
     @(posedge clk_i);
     dr_addr_i = 6'd0;
     #20;
     @(posedge clk_i);
     dr_addr_i = 6'bX;
     #80;
+    
     @(posedge clk_i);
     dr_addr_i = 6'd1;
     #20;
     @(posedge clk_i);
     dr_addr_i = 6'bX;
     #80;
+    
     @(posedge clk_i);
     dr_addr_i = 6'd2;
     #20;
     @(posedge clk_i);
     dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd3;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd4;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd5;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd6;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd7;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
+    @(posedge clk_i);
+    dr_addr_i = 6'd8;
+    #20;
+    @(posedge clk_i);
+    dr_addr_i = 6'bX;
+    #80;
+    
   end  
 
 enigma_wrapper ew(
 .clk_i         (clk_i),
-
 .rst_i         (rst_i),
-
 .symb_numb     (symb_numb),
-.input_s       (trans),
-
+.input_s       (trans1),
+.output_s      (trans2),
 .dr_we_i       (dr_we_i),
 .dr_data_i     (dr_data_i),
 .dr_addr_i     (dr_addr_i),
-.dr_data_o     (dr_data_o)
+.dr_data_o     (dr_data_o),
+.fr_data_o     (fr_data_o)
 );
 
 enigma_1 eg(
 .clk_i          (clk_i),
 .rst_i          (rst_i),
-.in_symb_i      (trans),
+.in_symb_i      (trans1),
 
-.out_symb_o     (out_symb_o)
+.out_symb_o     (trans2)
 );
 
 endmodule

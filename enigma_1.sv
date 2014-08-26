45b6287c35dc5a8f84b13d426c14e70fbc7de48e
zakdam <zakirov110594@gmail.com>
2014-08-26 21:07:31 +0400

    Update enigma_1.sv

--- enigma_1.sv
+++ enigma_1.sv
@@ -1,16 +1,16 @@
 module enigma_1
-  
+
 #(
-parameter R1_PAR = 1,
-parameter R2_PAR = 1,
-parameter R3_PAR = 1
+	parameter R1_PAR = 1,
+	parameter R2_PAR = 1,
+	parameter R3_PAR = 1
 )
 
 (
-input logic        clk_i,       //synchrosignal
-input logic        rst_i,       //asynchronous reset
-input logic  [6:0] in_symb_i,   //incoming NOT CODED letter, each letter is coded by its serial number
-output logic [6:0] out_symb_o   //outcoming CODED letter, each letter is coded by its serial number
+	input logic        clk_i,       //synchrosignal
+	input logic        rst_i,       //asynchronous reset
+	input logic  [6:0] in_symb_i,   //incoming NOT CODED letter, each letter is coded by its serial number
+	output logic [6:0] out_symb_o   //outcoming CODED letter, each letter is coded by its serial number
 );
 
 //in-logic of ALL blocks (dominoes principal)
@@ -40,16 +40,41 @@ logic signed [6:0] out_r3_backsymb;
 
 logic signed [6:0] out_presymb;
 
-logic signed [6:0] abs1;
-logic signed [6:0] abs2;
-logic signed [6:0] abs3;
-logic signed [6:0] abs4;
-logic signed [6:0] abs5;
-logic signed [6:0] abs6;
+logic signed [6:0] str_r1_array [0:26] = '{ 0,2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15 };
+
+logic signed [6:0] back_r1_array [0:26] = '{ 0,20,1,7,2,16,3,19,4,17,5,21,6,22,14,26,8,25,9,24,10,23,12,18,11,15,13 };
+
+logic signed [6:0] str_r2_array [0:26] = '{ 0,1,10,4,11,19,9,18,21,24,2,12,8,23,20,13,3,17,7,26,14,16,25,6,22,15,5 };
+
+logic signed [6:0] back_r2_array [0:26] = '{ 0,1,10,16,3,26,23,18,12,6,2,4,11,15,20,25,21,17,7,5,14,8,24,13,9,22,19 };
+
+logic signed [6:0] str_r3_array [0:26] = '{ 0,5,11,13,6,12,7,4,17,22,26,14,20,15,23,25,8,24,21,19,16,1,9,2,18,3,10 };
+
+logic signed [6:0] back_r3_array [0:26] = '{ 0,21,23,25,7,1,4,6,16,22,26,2,5,3,11,13,20,8,24,19,12,18,9,14,17,15,10 };
+
+logic signed [6:0] refl_array [0:26] = '{ 0,25,18,21,8,17,19,12,4,16,24,14,7,15,11,13,9,5,2,6,26,3,23,22,10,1,20 };
+
+//calculating symbol function
+function signed [6:0] symb_calc(input logic signed [6:0] a);
+logic signed [6:0] abs;
+	begin
+		if ( a < 1 )
+			begin
+				abs = a + 26;
+				if ( abs > 0 )
+					return abs;
+				else if ( abs < 0 )
+					return 0 - abs;
+			end
+		else if ( a > 26 )
+			return a - 26;
+		else 
+			return a;
+	end
+endfunction	
 
 //GATE BLOCK INPUT AND OUTPUT OF THE MACHINE
 //gate incoming symbol addition operation (G + R1); (trigger is needed for r1 counter's trigger delay compensation)
-
 assign in_r1_presymb = in_symb_i + r1;
 
 always_ff @ (posedge clk_i or negedge rst_i)
@@ -57,44 +82,11 @@ begin
   if ( !rst_i )
     in_r1_symb <= 0;
   else if ( (in_symb_i > 0) && (in_symb_i < 27) )
-    begin
-      if ( in_r1_presymb < 1 )
-        begin
-        abs1 = in_r1_presymb + 26;
-          if ( abs1 > 0 )
-            in_r1_symb = abs1;
-          else if ( abs1 < 0 )
-            in_r1_symb = 0 - abs1;
-        end
-      else if ( in_r1_presymb > 26 )
-        in_r1_symb <= in_r1_presymb - 26;
-      else 
-        in_r1_symb <= in_r1_presymb;
-    end
+		in_r1_symb <= symb_calc(in_r1_presymb);
 end
-
-//gate outcoming symbol subtraction operation (R1I - R1)
 
 assign out_presymb = out_r1_backsymb - (r1 - 1);
-
-always_comb
-begin
-  if ( (out_r1_backsymb > 0) && (out_r1_backsymb < 27) )
-    begin
-      if ( out_presymb < 1 )
-        begin
-        abs2 = out_presymb + 26;
-          if ( abs2 > 0 )
-            out_symb_o = abs2;
-          else if ( abs2 < 0 )
-            out_symb_o = 0 - abs2;
-        end
-      else if ( out_presymb > 26 )
-        out_symb_o = out_presymb - 26;
-      else
-        out_symb_o = out_presymb;
-    end
-end
+assign out_symb_o = ( (out_r1_backsymb > 0) && (out_r1_backsymb < 27) ) ? symb_calc(out_presymb) : 0;
 
 
 //ROTOR I BLOCK
@@ -110,115 +102,15 @@ begin
 end
 
 //special rotor I action
-always_comb
-begin
-  case ( in_r1_symb[4:0] )
-    7'd1    : out_r1_symb = 7'd2;
-    7'd2    : out_r1_symb = 7'd4;
-    7'd3    : out_r1_symb = 7'd6;
-    7'd4    : out_r1_symb = 7'd8;
-    7'd5    : out_r1_symb = 7'd10;
-    7'd6    : out_r1_symb = 7'd12;
-    7'd7    : out_r1_symb = 7'd3;
-    7'd8    : out_r1_symb = 7'd16;
-    7'd9    : out_r1_symb = 7'd18;
-    7'd10   : out_r1_symb = 7'd20;
-    7'd11   : out_r1_symb = 7'd24;
-    7'd12   : out_r1_symb = 7'd22;
-    7'd13   : out_r1_symb = 7'd26;
-    7'd14   : out_r1_symb = 7'd14;
-    7'd15   : out_r1_symb = 7'd25;
-    7'd16   : out_r1_symb = 7'd5;
-    7'd17   : out_r1_symb = 7'd9;
-    7'd18   : out_r1_symb = 7'd23;
-    7'd19   : out_r1_symb = 7'd7;
-    7'd20   : out_r1_symb = 7'd1;
-    7'd21   : out_r1_symb = 7'd11;
-    7'd22   : out_r1_symb = 7'd13;
-    7'd23   : out_r1_symb = 7'd21;
-    7'd24   : out_r1_symb = 7'd19;
-    7'd25   : out_r1_symb = 7'd17;
-    7'd26   : out_r1_symb = 7'd15;
-    default : out_r1_symb = 7'd0;
-  endcase
-end
+assign out_r1_symb = str_r1_array[in_r1_symb];
 
 assign in_r2_presymb = out_r1_symb + (r2 - r1);
-
-always_comb
-begin
-  if ( (out_r1_symb > 0) && (out_r1_symb < 27) )
-    begin
-      if ( in_r2_presymb < 1 )
-        begin
-        abs3 = in_r2_presymb + 26;
-          if ( abs3 > 0 )
-            in_r2_symb = abs3;
-          else if ( abs3 < 0 )
-            in_r2_symb = 0 - abs3;
-        end
-      else if ( in_r2_presymb > 26 )
-        in_r2_symb = in_r2_presymb - 26;
-      else
-        in_r2_symb = in_r2_presymb;
-    end
-end
-
+assign in_r2_symb = ( (out_r1_symb > 0) && (out_r1_symb < 27) ) ? symb_calc(in_r2_presymb) : 0; 
 assign in_r1_prebacksymb = out_r2_backsymb - (r2 - r1);
-
-always_comb
-begin
-  if ( (out_r2_backsymb > 0) && (out_r2_backsymb < 27) )
-    begin
-      if ( in_r1_prebacksymb < 1 )
-        begin
-        abs4 = in_r1_prebacksymb + 26;
-          if ( abs4 > 0 )
-            in_r1_backsymb = abs4;
-          else if ( abs4 < 0 )
-            in_r1_backsymb = 0 - abs4;
-        end
-      else if ( in_r1_prebacksymb > 26 )
-        in_r1_backsymb = in_r1_prebacksymb - 26;
-      else 
-        in_r1_backsymb = in_r1_prebacksymb;
-    end
-end
+assign in_r1_backsymb = ( (out_r2_backsymb > 0) && (out_r2_backsymb < 27) ) ? symb_calc(in_r1_prebacksymb) : 0;
 
 //special rotor I REVERSE action
-always_comb
-begin
-  case ( in_r1_backsymb[4:0] )
-    7'd1    : out_r1_backsymb = 7'd20;
-    7'd2    : out_r1_backsymb = 7'd1;
-    7'd3    : out_r1_backsymb = 7'd7;
-    7'd4    : out_r1_backsymb = 7'd2;
-    7'd5    : out_r1_backsymb = 7'd16;
-    7'd6    : out_r1_backsymb = 7'd3;
-    7'd7    : out_r1_backsymb = 7'd19;
-    7'd8    : out_r1_backsymb = 7'd4;
-    7'd9    : out_r1_backsymb = 7'd17;
-    7'd10   : out_r1_backsymb = 7'd5;
-    7'd11   : out_r1_backsymb = 7'd21;
-    7'd12   : out_r1_backsymb = 7'd6;
-    7'd13   : out_r1_backsymb = 7'd22;
-    7'd14   : out_r1_backsymb = 7'd14;
-    7'd15   : out_r1_backsymb = 7'd26;
-    7'd16   : out_r1_backsymb = 7'd8;
-    7'd17   : out_r1_backsymb = 7'd25;
-    7'd18   : out_r1_backsymb = 7'd9;
-    7'd19   : out_r1_backsymb = 7'd24;
-    7'd20   : out_r1_backsymb = 7'd10;
-    7'd21   : out_r1_backsymb = 7'd23;
-    7'd22   : out_r1_backsymb = 7'd12;
-    7'd23   : out_r1_backsymb = 7'd18;
-    7'd24   : out_r1_backsymb = 7'd11;
-    7'd25   : out_r1_backsymb = 7'd15;
-    7'd26   : out_r1_backsymb = 7'd13;
-    default : out_r1_backsymb = 7'd0;
-  endcase
-end
-
+assign out_r1_backsymb = back_r1_array[in_r1_backsymb];
 
 //ROTOR II BLOCK
 //r2 - rotor definition (counter)
@@ -233,115 +125,15 @@ begin
 end
 
 //special rotor II action
-always_comb
-begin
-  case ( in_r2_symb[4:0] )
-    7'd1    : out_r2_symb = 7'd1;
-    7'd2    : out_r2_symb = 7'd10;
-    7'd3    : out_r2_symb = 7'd4;
-    7'd4    : out_r2_symb = 7'd11;
-    7'd5    : out_r2_symb = 7'd19;
-    7'd6    : out_r2_symb = 7'd9;
-    7'd7    : out_r2_symb = 7'd18;
-    7'd8    : out_r2_symb = 7'd21;
-    7'd9    : out_r2_symb = 7'd24;
-    7'd10   : out_r2_symb = 7'd2;
-    7'd11   : out_r2_symb = 7'd12;
-    7'd12   : out_r2_symb = 7'd8;
-    7'd13   : out_r2_symb = 7'd23;
-    7'd14   : out_r2_symb = 7'd20;
-    7'd15   : out_r2_symb = 7'd13;
-    7'd16   : out_r2_symb = 7'd3;
-    7'd17   : out_r2_symb = 7'd17;
-    7'd18   : out_r2_symb = 7'd7;
-    7'd19   : out_r2_symb = 7'd26;
-    7'd20   : out_r2_symb = 7'd14;
-    7'd21   : out_r2_symb = 7'd16;
-    7'd22   : out_r2_symb = 7'd25;
-    7'd23   : out_r2_symb = 7'd6;
-    7'd24   : out_r2_symb = 7'd22;
-    7'd25   : out_r2_symb = 7'd15;
-    7'd26   : out_r2_symb = 7'd5;
-    default : out_r2_symb = 7'd0; 
-  endcase
-end
+assign out_r2_symb = str_r2_array[in_r2_symb];
 
 assign in_r3_presymb = out_r2_symb + (r3 - r2);
-
-always_comb
-begin
-  if ( (out_r2_symb > 0) && (out_r2_symb < 27) )
-    begin
-      if ( in_r3_presymb < 1 )
-        begin
-        abs5 = in_r3_presymb + 26;
-          if ( abs5 > 0 )
-            in_r3_symb = abs5;
-          else if ( abs5 < 0 )
-            in_r3_symb = 0 - abs5;
-        end
-      else if ( in_r3_presymb > 26 )
-        in_r3_symb = in_r3_presymb - 26;
-      else
-        in_r3_symb = in_r3_presymb;
-    end
-end
-
+assign in_r3_symb = ( (out_r2_symb > 0) && (out_r2_symb < 27) ) ? symb_calc(in_r3_presymb) : 0;
 assign in_r2_prebacksymb = out_r3_backsymb - (r3 - r2);
-
-always_comb
-begin
-  if ( (out_r3_backsymb > 0) && (out_r3_backsymb < 27) )
-    begin
-      if ( in_r2_prebacksymb < 1 )
-        begin
-        abs6 = in_r2_prebacksymb + 26;
-          if ( abs6 > 0 )
-            in_r2_backsymb = abs6;
-          else if ( abs6 < 0 )
-            in_r2_backsymb = 0 - abs6;
-        end
-      else if ( in_r2_prebacksymb > 26 )
-        in_r2_backsymb = in_r2_prebacksymb - 26;
-      else
-        in_r2_backsymb = in_r2_prebacksymb;
-    end
-end
+assign in_r2_backsymb = ( (out_r3_backsymb > 0) && (out_r3_backsymb < 27) ) ? symb_calc(in_r2_prebacksymb) : 0;
 
 //special rotor II REVERSE action
-always_comb
-begin
-  case ( in_r2_backsymb[4:0] )
-    7'd1    : out_r2_backsymb = 7'd1;
-    7'd2    : out_r2_backsymb = 7'd10;
-    7'd3    : out_r2_backsymb = 7'd16;
-    7'd4    : out_r2_backsymb = 7'd3;
-    7'd5    : out_r2_backsymb = 7'd26;
-    7'd6    : out_r2_backsymb = 7'd23;
-    7'd7    : out_r2_backsymb = 7'd18;
-    7'd8    : out_r2_backsymb = 7'd12;
-    7'd9    : out_r2_backsymb = 7'd6;
-    7'd10   : out_r2_backsymb = 7'd2;
-    7'd11   : out_r2_backsymb = 7'd4;
-    7'd12   : out_r2_backsymb = 7'd11;
-    7'd13   : out_r2_backsymb = 7'd15;
-    7'd14   : out_r2_backsymb = 7'd20;
-    7'd15   : out_r2_backsymb = 7'd25;
-    7'd16   : out_r2_backsymb = 7'd21;
-    7'd17   : out_r2_backsymb = 7'd17;
-    7'd18   : out_r2_backsymb = 7'd7;
-    7'd19   : out_r2_backsymb = 7'd5;
-    7'd20   : out_r2_backsymb = 7'd14;
-    7'd21   : out_r2_backsymb = 7'd8;
-    7'd22   : out_r2_backsymb = 7'd24;
-    7'd23   : out_r2_backsymb = 7'd13;
-    7'd24   : out_r2_backsymb = 7'd9;
-    7'd25   : out_r2_backsymb = 7'd22;
-    7'd26   : out_r2_backsymb = 7'd19;
-    default : out_r2_backsymb = 7'd0;
-  endcase
-end
-
+assign out_r2_backsymb = back_r2_array [in_r2_backsymb];
 
 //ROTOR III BLOCK
 //r3 - rotor definition (counter)
@@ -356,106 +148,14 @@ begin
 end
 
 //special rotor III action
-always_comb
-begin
-  case ( in_r3_symb[4:0] )
-    7'd1    : out_r3_symb = 7'd5;
-    7'd2    : out_r3_symb = 7'd11;
-    7'd3    : out_r3_symb = 7'd13;
-    7'd4    : out_r3_symb = 7'd6;
-    7'd5    : out_r3_symb = 7'd12;
-    7'd6    : out_r3_symb = 7'd7;
-    7'd7    : out_r3_symb = 7'd4;
-    7'd8    : out_r3_symb = 7'd17;
-    7'd9    : out_r3_symb = 7'd22;
-    7'd10   : out_r3_symb = 7'd26;
-    7'd11   : out_r3_symb = 7'd14;
-    7'd12   : out_r3_symb = 7'd20;
-    7'd13   : out_r3_symb = 7'd15;
-    7'd14   : out_r3_symb = 7'd23;
-    7'd15   : out_r3_symb = 7'd25;
-    7'd16   : out_r3_symb = 7'd8;
-    7'd17   : out_r3_symb = 7'd24;
-    7'd18   : out_r3_symb = 7'd21;
-    7'd19   : out_r3_symb = 7'd19;
-    7'd20   : out_r3_symb = 7'd16;
-    7'd21   : out_r3_symb = 7'd1;
-    7'd22   : out_r3_symb = 7'd9;
-    7'd23   : out_r3_symb = 7'd2;
-    7'd24   : out_r3_symb = 7'd18;
-    7'd25   : out_r3_symb = 7'd3;
-    7'd26   : out_r3_symb = 7'd10;
-    default : out_r3_symb = 7'd0;
-  endcase
-end
+assign out_r3_symb = str_r3_array[in_r3_symb];
 
 //special rotor III REVERSE action
-always_comb
-begin
-  case ( in_r3_backsymb[4:0] )
-    7'd1    : out_r3_backsymb = 7'd21;
-    7'd2    : out_r3_backsymb = 7'd23;
-    7'd3    : out_r3_backsymb = 7'd25;
-    7'd4    : out_r3_backsymb = 7'd7;
-    7'd5    : out_r3_backsymb = 7'd1;
-    7'd6    : out_r3_backsymb = 7'd4;
-    7'd7    : out_r3_backsymb = 7'd6;
-    7'd8    : out_r3_backsymb = 7'd16;
-    7'd9    : out_r3_backsymb = 7'd22;
-    7'd10   : out_r3_backsymb = 7'd26;
-    7'd11   : out_r3_backsymb = 7'd2;
-    7'd12   : out_r3_backsymb = 7'd5;
-    7'd13   : out_r3_backsymb = 7'd3;
-    7'd14   : out_r3_backsymb = 7'd11;
-    7'd15   : out_r3_backsymb = 7'd13;
-    7'd16   : out_r3_backsymb = 7'd20;
-    7'd17   : out_r3_backsymb = 7'd8;
-    7'd18   : out_r3_backsymb = 7'd24;
-    7'd19   : out_r3_backsymb = 7'd19;
-    7'd20   : out_r3_backsymb = 7'd12;
-    7'd21   : out_r3_backsymb = 7'd18;
-    7'd22   : out_r3_backsymb = 7'd9;
-    7'd23   : out_r3_backsymb = 7'd14;
-    7'd24   : out_r3_backsymb = 7'd17;
-    7'd25   : out_r3_backsymb = 7'd15;
-    7'd26   : out_r3_backsymb = 7'd10;
-    default : out_r3_backsymb = 7'd0;
-  endcase
-end
+assign out_r3_backsymb = back_r3_array[in_r3_backsymb];
+
 
 //REFLECTOR BLOCK
 //special reflector action
-always_comb
-begin
-  case ( out_r3_symb[4:0] )
-    7'd1    : in_r3_backsymb = 7'd25;
-    7'd2    : in_r3_backsymb = 7'd18;
-    7'd3    : in_r3_backsymb = 7'd21;
-    7'd4    : in_r3_backsymb = 7'd8;
-    7'd5    : in_r3_backsymb = 7'd17;
-    7'd6    : in_r3_backsymb = 7'd19;
-    7'd7    : in_r3_backsymb = 7'd12;
-    7'd8    : in_r3_backsymb = 7'd4;
-    7'd9    : in_r3_backsymb = 7'd16;
-    7'd10   : in_r3_backsymb = 7'd24;
-    7'd11   : in_r3_backsymb = 7'd14;
-    7'd12   : in_r3_backsymb = 7'd7;
-    7'd13   : in_r3_backsymb = 7'd15;
-    7'd14   : in_r3_backsymb = 7'd11;
-    7'd15   : in_r3_backsymb = 7'd13;
-    7'd16   : in_r3_backsymb = 7'd9;
-    7'd17   : in_r3_backsymb = 7'd5;
-    7'd18   : in_r3_backsymb = 7'd2;
-    7'd19   : in_r3_backsymb = 7'd6;
-    7'd20   : in_r3_backsymb = 7'd26;
-    7'd21   : in_r3_backsymb = 7'd3;
-    7'd22   : in_r3_backsymb = 7'd23;
-    7'd23   : in_r3_backsymb = 7'd22;
-    7'd24   : in_r3_backsymb = 7'd10;
-    7'd25   : in_r3_backsymb = 7'd1;
-    7'd26   : in_r3_backsymb = 7'd20;
-    default : in_r3_backsymb = 7'd0;
-  endcase
-end
+assign in_r3_backsymb = refl_array[out_r3_symb];
 
 endmodule

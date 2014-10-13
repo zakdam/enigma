d2ca5119f76044e7c4cd143876ef8832548d8f15
zakdam <zakirov110594@gmail.com>
2014-10-13 11:07:22 +0400

    Update enigma_top.sv

--- enigma_top.sv
+++ enigma_top.sv
@@ -20,6 +20,17 @@ logic signed [6:0] cod_symb_trans;          // coded symbol transit between out_
 logic              noncod_val_trans;        // non-coded symbol validity transit between en_val_o and en_val_i
 logic              cod_val_trans;           // coded symbol validity transit between encod_val_o and encod_val_i
 
+// transit ports to connect enigma_1 and rotors
+logic      [6:0] r1_trans;
+logic [5:1][6:0] r1_trans_d;
+
+logic      [6:0] r2_trans;
+logic [4:1][6:0] r2_trans_d;
+
+logic      [6:0] r3_trans;
+logic [3:1][6:0] r3_trans_d;
+
+
 // portmap for wrapper
 wrapper wr(
   .clk_i          ( clk_i             ),
@@ -37,6 +48,7 @@ wrapper wr(
   .en_val_o       ( noncod_val_trans  )
 );
 
+
 // portmap for enigma_1
 enigma_1 en(
   .clk_i          ( clk_i             ),
@@ -44,9 +56,33 @@ enigma_1 en(
   .en_val_i       ( noncod_val_trans  ),
   .rotors_rst_i   ( rotors_rst_i      ),
   .in_symb_i      ( noncod_symb_trans ),
+  
+  .r1_i           ( r1_trans          ),
+  .r1_d_i         ( r1_trans_d        ),
+  .r2_i           ( r2_trans          ),
+  .r2_d_i         ( r2_trans_d        ),
+  .r3_i           ( r3_trans          ),
+  .r3_d_i         ( r3_trans_d        ),
 
   .out_symb_o     ( cod_symb_trans    ),
   .encod_val_o    ( cod_val_trans     )
 );
+
+
+// portmap for rotors
+rotors rs(
+  .clk_i          ( clk_i        ),
+  .rst_i          ( rst_i        ),
+  .rotors_rst_i   ( rotors_rst_i ),
+  .in_symb_val_i  ( symb_val_i   ),
+
+  .r1_o           ( r1_trans     ),
+  .r1_d_o         ( r1_trans_d   ),
+  .r2_o           ( r2_trans     ),
+  .r2_d_o         ( r2_trans_d   ),
+  .r3_o           ( r3_trans     ),
+  .r3_d_o         ( r3_trans_d   )
+);
+
 
 endmodule
